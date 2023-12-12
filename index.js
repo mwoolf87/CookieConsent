6342610a377143c1112d721e1abcd6df4551dd5b
mwoolf87 <mwoolfdev@gmail.com>
2023-12-11 21:39:23 -0500

    added javascript to close modal

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const modal = document.getElementById("modal");
+const modalCloseBtn = document.getElementById("modal-close-btn");
 
 setTimeout(function () {
   modal.style.display = "inline";
 }, 1500);
+
+closeBtn.addEventListener("click", function () {
+  modalCloseBtn.style.display = "none";
+});
